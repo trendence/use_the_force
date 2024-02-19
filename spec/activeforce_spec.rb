8af461f5043b8ac74d5d5d67c228cdaa5038e3a1
IvanSchlagheck <ivansch05@gmail.com>
2024-02-19 15:22:53 +0100

    implemented bundler boilerplate

---
+++ spec/activeforce_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Activeforce do
+  it "has a version number" do
+    expect(Activeforce::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
